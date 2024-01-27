7cb2c5b7dad3d0865f3b60dde144abc55e6d813f
KelvynLenis <kmartinslenis30@hotmail.com>
2024-01-27 16:26:39 -0300

    uploading project

--- app/api/webhook/clerk/route.ts
+++ app/api/webhook/clerk/route.ts
@@ -1,9 +1,9 @@
-import { Webhook } from 'svix';
+import { Webhook } from 'svix'
 import { headers } from 'next/headers'
 import { WebhookEvent } from '@clerk/nextjs/server'
-import { createUser, deleteUser, updateUser } from '@/lib/actions/user.action';
-import { clerkClient } from '@clerk/nextjs';
-import { NextResponse } from 'next/server';
+import { createUser, deleteUser, updateUser } from '@/lib/actions/user.actions'
+import { clerkClient } from '@clerk/nextjs'
+import { NextResponse } from 'next/server'
  
 export async function POST(req: Request) {
  
@@ -56,14 +56,14 @@ export async function POST(req: Request) {
  
   if(eventType === 'user.created') {
     const { id, email_addresses, image_url, first_name, last_name, username } = evt.data;
-    
+
     const user = {
       clerkId: id,
       email: email_addresses[0].email_address,
       username: username!,
       firstName: first_name,
       lastName: last_name,
-      photo: image_url
+      photo: image_url,
     }
 
     const newUser = await createUser(user);
